274a5817aaf5682de9b94cc1698ab3cb0dc31a0b
ProXxNebula <T3chN3bulaX@protonmail.com>
2025-12-18 09:35:35 +1300

    Add service enumeration in ./enum.sh & change grep to awk for parsing open ports.

--- bin/enum.sh
+++ bin/enum.sh
@@ -1,21 +1,38 @@
 #!/usr/bin/env bash
-
+# Exit on error
 set -euo pipefail
 
+# Variables
 TARGET="$1"
 PORTS="$2"
 OUTDIR="recon/$TARGET"
 
+# Creating Directory
 mkdir -p "$OUTDIR"
 
+# Checking the target if alive
 echo "Ping to $TARGET"
 ping -c 1 "$TARGET" >/dev/null && echo "Alive" | tee "$OUTDIR/alive.txt" 
 
+# Checking for open ports
 echo "Scanning $TARGET"
 nmap -T4 --top-ports "$PORTS" "$TARGET" 2>&1 | tee "$OUTDIR/nmap.txt" 
 
-echo "Getting Open Ports"
-: > "$OUTDIR/open_ports.txt"
-grep "open" "$OUTDIR/nmap.txt" >> "$OUTDIR/open_ports.txt" || true
+# Preparing open ports for service enumeration 
+echo "Preparing open ports (CSV)"
+
+awk '/open/ { split($1,a,"/"); print a[1] }' "$OUTDIR/nmap.txt" \
+    | sort -n -u \
+    | paste -sd, - \
+    > "$OUTDIR/open_ports.csv"
+
+# Service Enumeration
+if [[ -s "$OUTDIR/open_ports.csv" ]]; then
+    echo "Running service enumeration"
+    nmap -sV -p "$(cat "$OUTDIR/open_ports.csv")" "$TARGET" \
+        | tee "$OUTDIR/services.txt"
+else
+    echo "No open ports found"
+fi
 
 
