31c51966559c1780ca9408cfa9ca659b6d970754
ProXxNebula <T3chN3bulaX@protonmail.com>
2025-12-19 09:44:41 +1300

    Update comment on finder.sh for testing version, to specify HTTP/HTTPS test

--- bin/finder.sh
+++ bin/finder.sh
@@ -5,7 +5,7 @@ TARGET="$1"
 PORTS=$(tr ',' ' ' < "recon/$TARGET/open_ports.csv")
 OUTDIR="recon/$TARGET"
 
-# Testing version
+# Testing version for HTTP AND HTTPS
 for p in $(echo "$PORTS" | tr ',' ' '); do
     curl -I "$TARGET:$p" | tee "$OUTDIR/finder_headers.txt"
 done
